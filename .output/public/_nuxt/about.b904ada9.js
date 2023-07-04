b2823ad2376170a752f1f0e8d106a3127b6b3df1
Savastane <savastane@gmail.com>
2023-07-03 22:26:40 -0300

    statr 3 SSR

---
+++ .output/public/_nuxt/about.b904ada9.js
@@ -0,0 +1,1 @@
+import{d as e,k as t,l as o,m as s}from"./entry.f76ac7b9.js";const a=s("h1",null,"teste about",-1),n=[a],u=e({__name:"about",setup(_){return(c,r)=>(t(),o("div",null,n))}});export{u as default};
--- .output/public/_nuxt/index.1911383f.js
+++
@@ -1,1 +0,0 @@
-import{a as e}from"./entry.2653affb.js";const a=e({__name:"index",setup(n){return(t,r)=>null}});export{a as default};
